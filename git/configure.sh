217bf4f988c8d334fc17503380afb26b6269c2a0
Sujoy Gupta <sujoy@topprospect.com>
2010-12-02 18:04:41 -0800

    Script for default git configuration

---
+++ git/configure.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+git config --global alias.st "status"
+git config --global alias.co "checkout"
+
+git config --global branch.autosetupmerge true
+git config --global push.default current
+
+git config --global color.branch auto
+git config --global color.diff auto
+git config --global color.interactive auto
+git config --global color.status auto
+
